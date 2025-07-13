75935f13bc26612e906900601ef9fa568ff32c1e
Thiago Wanderley de Amorim <thiago.wander22@gmail.com>
2025-07-12 22:40:44 -0300

    chore: add tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint'],
+  modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss', '@nuxt/image'],
   runtimeConfig: {
     public: {
-      apiBase: process.env.API_BASE_URL
+      apiBase: process.env.API_BASE_URL || 'http://localhost:8000'
     }
   },
 })
