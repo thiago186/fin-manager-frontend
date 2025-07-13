0ee44a658762da4817f03fff933f0d1e9c1b13b7
Thiago Wanderley de Amorim <thiago.wander22@gmail.com>
2025-07-12 21:47:13 -0300

    chore: add base api url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
+  modules: ['@nuxt/eslint'],
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.API_BASE_URL
+    }
+  },
 })
