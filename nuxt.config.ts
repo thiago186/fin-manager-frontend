a8806971cd18d8e4af5172d445b2649af08cc5da
Thiago Wanderley de Amorim <thiago.wander22@gmail.com>
2025-12-23 16:47:17 -0300

    feat: add configuration for Shadcn components and Tailwind CSS integration with utility functions

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,35 @@
+import tailwindcss from '@tailwindcss/vite'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss', '@nuxt/image'],
+  modules: [
+    '@nuxt/eslint',
+    '@nuxtjs/tailwindcss',
+    '@nuxt/image',
+    'shadcn-nuxt',
+  ],
+  css: ['~/assets/css/tailwind.css'],
+  shadcn: {
+    /**
+     * Prefix for all the imported component.
+     * @default "Ui"
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * Will respect the Nuxt aliases.
+     * @link https://nuxt.com/docs/api/nuxt-config#alias
+     * @default "@/components/ui"
+     */
+    componentDir: '@/components/ui'
+  },
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
   runtimeConfig: {
     public: {
       apiBase: process.env.API_BASE_URL || 'http://localhost:8000'
